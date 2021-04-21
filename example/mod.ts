8be82f87160638fe2502a083c3c721f3b6f5abcb
Matheus Vinícius <mtwzim@gmail.com>
2021-04-21 12:20:22 -0300

    feat: run command, fix: dyno map for path dependencies

--- example/mod.ts
+++ example/mod.ts
@@ -1,8 +1,8 @@
-import { serve } from "http/server.ts";
+import { serve } from "@std/http";
 const s = serve({ port: 8000 });
 
 console.log("http://localhost:8000/");
 
 for await (const req of s) {
-    req.respond({ body: "Hello World\n" });
+  req.respond({ body: "Hello World\n" });
 }
